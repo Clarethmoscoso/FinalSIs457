c240f79fe1b60efdab52e110ac4ac299a54619d5
Clarethmoscoso <114823610+Clarethmoscoso@users.noreply.github.com>
2023-01-14 11:09:29 -0400

    Add files via upload

---
+++ FinalSis457Caemu/Models/Serie.cs
@@ -0,0 +1,20 @@
+﻿namespace FinalSis457Caemu.Models;
+
+public partial class Serie
+{
+    public int Id { get; set; }
+
+    public string Titulo { get; set; } = null!;
+
+    public string Sinopsis { get; set; } = null!;
+
+    public string Director { get; set; } = null!;
+
+    public int Duracion { get; set; }
+
+    public DateTime FechaEstreno { get; set; }
+
+    public string? UsuarioRegistro { get; set; }
+
+    public bool? RegistroActivo { get; set; }
+}
